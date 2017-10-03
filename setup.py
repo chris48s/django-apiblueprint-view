4c65002ac88701a1e208cdbbd7e3888175ff5249
chris48s <chris.shaw480@gmail.com>
2017-10-03 09:20:23 +0100

    bump version to 1.1.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-apiblueprint-view',
-    version='1.0.0',
+    version='1.1.0',
     author="chris48s",
     license="MIT",
     packages=find_packages(),
