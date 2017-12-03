3d569a0173fb07b33b6dd64f8c4fcbbdda9d034c
chris48s <chris.shaw480@gmail.com>
2017-12-03 14:01:07 +0000

    add package meta data

--- setup.py
+++ setup.py
@@ -26,5 +26,19 @@ setup(
         'draughtsman',
         'markdown2',
         'typing',
-    ]
+    ],
+    classifiers=[
+        'Framework :: Django',
+        'Framework :: Django :: 1.8',
+        'Framework :: Django :: 1.9',
+        'Framework :: Django :: 1.10',
+        'Framework :: Django :: 1.11',
+        'Framework :: Django :: 2.0',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
 )
