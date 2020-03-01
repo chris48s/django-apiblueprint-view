6875d2a487018ca878fa9252970db31c8d937b7c
chris48s <chris.shaw480@gmail.com>
2020-03-01 21:09:37 +0000

    publish wheels to pypi on master push with tag

--- .github/actions/build-wheels/entrypoint.sh
+++ .github/actions/build-wheels/entrypoint.sh
@@ -8,5 +8,8 @@ yum install -y wget
 /opt/python/cp36-cp36m/bin/poetry build -f wheel
 
 for whl in ./dist/*.whl; do
-  auditwheel repair $whl --plat "manylinux1_x86_64"
+  auditwheel repair $whl --plat "manylinux1_x86_64" -w ./dist
 done
+
+/opt/python/cp36-cp36m/bin/poetry config pypi-token.pypi "$1"
+/opt/python/cp36-cp36m/bin/poetry publish -vvv -n
