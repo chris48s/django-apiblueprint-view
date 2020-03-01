9889e7d80c4745a3b8023e92a6ad513f68eab84e
chris48s <chris.shaw480@gmail.com>
2020-03-01 15:49:07 +0000

    GitHub action to build wheel in container

---
+++ .github/actions/build-wheels/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -e
+
+yum install -y wget
+/opt/python/cp36-cp36m/bin/pip install poetry
+./build-drafter.sh
+/opt/python/cp36-cp36m/bin/poetry install
+/opt/python/cp36-cp36m/bin/poetry build -f wheel
+
+for whl in ./dist/*.whl; do
+  auditwheel repair $whl --plat "manylinux1_x86_64"
+done
