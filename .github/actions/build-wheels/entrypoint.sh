8b700eab1ca6c40409c22d21c4e3dbc9eea6d15b
chris48s <chris.shaw480@gmail.com>
2020-04-18 16:45:21 +0100

    audit linux_x86_64

--- .github/actions/build-wheels/entrypoint.sh
+++ .github/actions/build-wheels/entrypoint.sh
@@ -13,7 +13,7 @@ yum install -y wget
 /opt/python/cp36-cp36m/bin/poetry build -f wheel
 
 for whl in ./dist/*.whl; do
-  auditwheel repair $whl --plat "manylinux1_x86_64" -w ./dist
+  auditwheel repair $whl --plat "linux_x86_64" -w ./dist
 done
 
 /opt/python/cp36-cp36m/bin/poetry config pypi-token.pypi "$1"
