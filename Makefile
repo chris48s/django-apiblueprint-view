ac453533615bb33a8285a2b3d654cc5899cbd5e5
chris48s <chris48s@users.noreply.github.com>
2023-10-17 20:34:13 +0100

    build a macosx wheel (#182)
    * build a macosx wheel

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ help:
 	@grep '^\.PHONY' Makefile | cut -d' ' -f2- | tr ' ' '\n'
 
 build-drafter:
-	./build-drafter.sh
+	./build-drafter.sh linux
 
 format:
 	poetry run isort .
--- build-drafter.sh
+++ build-drafter.sh
@@ -2,6 +2,13 @@
 
 set -euxo pipefail
 
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 [mac|linux]"
+    exit 1
+fi
+
+platform="$1"
+
 wget https://github.com/apiaryio/drafter/releases/download/v3.2.7/drafter-v3.2.7.tar.gz
 tar xvzf drafter-v3.2.7.tar.gz
 cd drafter-v3.2.7
@@ -9,6 +16,10 @@ python2.7 configure --shared
 make libdrafter
 cd ..
 mkdir -p apiblueprint_view/lib
-cp drafter-v3.2.7/build/out/Release/lib.target/libdrafter.so ./apiblueprint_view/lib/libdrafter.so
+if [ "$platform" = "mac" ]; then
+    cp drafter-v3.2.7/build/out/Release/libdrafter.dylib ./apiblueprint_view/lib/libdrafter.dylib
+else
+    cp drafter-v3.2.7/build/out/Release/lib.target/libdrafter.so ./apiblueprint_view/lib/libdrafter.so
+fi
 rm -rf drafter-v3.2.7/
 rm drafter-v3.2.7.tar.gz
